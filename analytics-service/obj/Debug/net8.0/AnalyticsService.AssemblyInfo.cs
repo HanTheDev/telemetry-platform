1cee142fe09a38443cd10749e022d4ef5099700e
“HanTheDev” <hanfirka1@gmail.com>
2025-12-08 00:52:30 +0700

    move the whole AnalyticsService test folder

--- analytics-service/obj/Debug/net8.0/AnalyticsService.AssemblyInfo.cs
+++ analytics-service/obj/Debug/net8.0/AnalyticsService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AnalyticsService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c46e53e82a8908cd68afbaa4d290d03a8af8df83")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4adc1d4dd78f6f94f0b5f5c7c84c291452882008")]
 [assembly: System.Reflection.AssemblyProductAttribute("AnalyticsService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AnalyticsService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

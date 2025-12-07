a16763ff7861ee1e67e06dbebf041ea353a221e3
“HanTheDev” <hanfirka1@gmail.com>
2025-12-08 01:02:07 +0700

    initialize docker-compose.yml

--- analytics-service/obj/Debug/net8.0/AnalyticsService.AssemblyInfo.cs
+++ analytics-service/obj/Debug/net8.0/AnalyticsService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AnalyticsService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00f1e7152d7f22383cff6458e36b38a6cd01b3b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbee8560eed58dea66acdf837060f42bd62834c7")]
 [assembly: System.Reflection.AssemblyProductAttribute("AnalyticsService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AnalyticsService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
